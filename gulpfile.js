66954e9117a76f6d7f2702ee2774ffa662250ad6
Levko Kravets <levko.ne@gmail.com>
2016-12-21 17:24:27 +0200

    Update structure

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ gulp.task('default', function() {
     path.join(nodeModulesDir, 'angular/angular.min.js'),
     path.join(nodeModulesDir, 'angular-tartan/dist/angular-tartan.min.js'),
     path.join(nodeModulesDir, 'tartan/dist/tartan.min.js'),
+    path.join(nodeModulesDir, 'tartan-processing/dist/tartan-processing.min.js'),
+    path.join(nodeModulesDir, 'tartan-render/dist/tartan-render.min.js'),
+    path.join(nodeModulesDir, 'tartan-schema/dist/tartan-schema.min.js'),
     path.join(nodeModulesDir, 'lodash/lodash.min.js'),
     path.join(nodeModulesDir, 'jquery/dist/jquery.min.js')
   ]).pipe(gulp.dest(assetsDir));
